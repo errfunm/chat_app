81094fbd2df75a05680b3cb8e75063ffaabba544
Erfan Baghi <er.baghi@gmail.com>
2023-11-29 14:28:37 +0330

    Message, PrivateChat, Participants, TextMessage models added.

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField()
+    bio = models.CharField(max_length=100)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Participants(models.Model):
+    users = models.ManyToManyField(User,)
+    date_created = models.DateTimeField(auto_now_add=True)
+    is_private = models.BooleanField(default=True)
+
+
+class PrivateChat(models.Model):
+    participants = models.OneToOneField(Participants, on_delete=models.CASCADE)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    conversation = models.ForeignKey(PrivateChat, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    content_type = models.CharField(max_length=20)
+
+
+class TextMessage(models.Model):
+    message = models.OneToOneField(Message, on_delete=models.CASCADE, primary_key=True)
+    text_content = models.TextField()
