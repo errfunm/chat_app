b6f34966ea3003a39417741527279503d1da4475
Erfan Baghi <er.baghi@gmail.com>
2023-11-29 15:05:22 +0330

    models registered in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
