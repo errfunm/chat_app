b9064bd60f5fdab34c8d0043952680fa11dac8e0
Erfan Baghi <er.baghi@gmail.com>
2023-12-01 14:23:19 +0330

    adding some styling.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="accounts/login.html", next_page="/"), name='login-view'),
-    path('logout/', LogoutView.as_view(next_page="login/"), name="logout-view"),
+    path('logout/', LogoutView.as_view(next_page="/"), name="logout-view"),
 ]
